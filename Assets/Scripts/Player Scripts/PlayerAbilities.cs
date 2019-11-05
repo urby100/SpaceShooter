c3572a6e3566822f694fe4a000d0ff91e9d45e0f
urby100 <urbanbozic1996@gmail.com>
2019-11-05 12:46:33 +0100

    Added Attack 2 - Laser

--- Assets/Scripts/Player Scripts/PlayerAbilities.cs
+++ Assets/Scripts/Player Scripts/PlayerAbilities.cs
@@ -10,10 +10,17 @@ public class PlayerAbilities : MonoBehaviour
     float attackSpeedAttack1 = 0.1f;
     float attack1NextAttack;
 
+    public GameObject attack2SpawnPoint;
+    public Transform attack2HitPoint;
+    private LineRenderer attack2LineRenderer;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        attack2LineRenderer = attack2SpawnPoint.GetComponent<LineRenderer>();
+        attack2LineRenderer.enabled = false;
+        attack2LineRenderer.useWorldSpace = true;
     }
     void FixedUpdate()
     {
@@ -23,5 +30,18 @@ public class PlayerAbilities : MonoBehaviour
                 attack1NextAttack = Time.time + attackSpeedAttack1;
             }
         }
+        RaycastHit2D hit = Physics2D.Raycast(attack2SpawnPoint.transform.position, attack2SpawnPoint.transform.up);
+        attack2HitPoint.position = attack2SpawnPoint.transform.up *20 + attack2SpawnPoint.transform.position;
+        attack2LineRenderer.SetPosition(0, attack2SpawnPoint.transform.position);
+        attack2LineRenderer.SetPosition(1, attack2HitPoint.position);
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            attack2LineRenderer.enabled = true;
+        }
+        else
+        {
+            attack2LineRenderer.enabled = false;
+        }
     }
+
 }
